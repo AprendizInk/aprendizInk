a90b8b437fa8ada52015bd2f85b5d980f8984f78
Juan Morales <juan.morales@newshore.es>
2023-04-04 14:42:12 -0500

    login and register form components structured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './main-page/login-page/login-page.component';
+import { LoginFormComponent } from './main-page/login-form/login-form.component';
+import { RegisterFormComponent } from './main-page/register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
