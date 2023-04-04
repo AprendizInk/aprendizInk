1e0af7809c430e0b8cd969baf76a4b0b2c316a52
Juan Morales <juan.morales@newshore.es>
2023-04-03 20:31:02 -0500

    global styles added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
