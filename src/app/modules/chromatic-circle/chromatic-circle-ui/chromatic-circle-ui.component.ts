305bffccba347bc6f8c82beb1690134c03e597c8
YonnySanchez <aprendizink@gmail.com>
2023-06-10 15:33:51 -0500

    chromatic circle create component

---
+++ src/app/modules/chromatic-circle/chromatic-circle-ui/chromatic-circle-ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chromatic-circle-ui',
+  templateUrl: './chromatic-circle-ui.component.html',
+  styleUrls: ['./chromatic-circle-ui.component.css']
+})
+export class ChromaticCircleUiComponent {
+
+}
